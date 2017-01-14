0eb3f53bdf0d6b5a32d5e3c9cd0637b6c1f86710
Gabriel Olivério <gabrieloliverio.py@gmail.com>
2017-01-13 23:13:27 -0200

    Deleted Metadata folder.

--- src/Metadata/Connector.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Bronco\LaravelGenerators\Metadata;
-
-interface Connector {
-    public function getColumns($databaseName, $tableTable);
-    public function getPrimaryKeys($databaseName, $tableTable);
-    public function getForeignKeys($databaseName, $tableTable);
-}
